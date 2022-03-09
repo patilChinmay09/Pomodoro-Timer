018356d681bad01025ca7b10bc9559e73335f9ff
patilChinmay09 <patilchinmay922@gmail.com>
2022-03-09 17:33:42 +0530

    Pomodoro Timer v1

--- Scripts/notification.js
+++
@@ -1,8 +0,0 @@
-function playNote(){
-    var task = document.getElementsByClassName("active-tsk")[0].getAttribute("id");
-    var task = JSON.parse(localStorage.getItem(task));
-    var alarmTone = task["alarm"];
-    var path = "../Audio/"+alarmTone+".wav";
-    var audio = new Audio(path);
-    audio.play();
-}
